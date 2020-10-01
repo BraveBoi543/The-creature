cd337b9e5af2491f6cbad9647805aef8bae4b378
Joseph Joestar <skylanders024@gmail.com>
2020-10-01 11:05:02 -0400

    Add project files.

---
+++ The creature/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace The_creature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Making Faces with Symbols
+            Console.WriteLine("c_     ___ ");
+            Console.WriteLine("  \\\\__|__|__");
+            Console.WriteLine("    \\( o_o)");
+            Console.WriteLine("     > ~  >");
+            Console.WriteLine("    / 　 / \\\\");
+            Console.WriteLine("　　 /　　/　 \\\\");
+            Console.WriteLine("　　 \\　 )　　c/");
+            Console.WriteLine("　　/　 /");
+            Console.WriteLine("　 /　/|");
+            Console.WriteLine("　(　( \\ ");
+            Console.WriteLine("　|　|  \\ ");
+            Console.WriteLine("　| / \\ )");
+            Console.WriteLine("　| |　) |");
+            Console.WriteLine(" /  )(_/");
+            Console.WriteLine("(_ /");
+            Console.WriteLine("He do be vibin Doe");
+        }
+    }
+}
